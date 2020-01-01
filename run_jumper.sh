1e6b3684569a54a4c98402c439a1ddca9c3bbc9d
Josh Roy <josh_roy@brown.edu>
2020-01-01 13:14:42 +0530

    not removing

--- run_jumper.sh
+++ run_jumper.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # rm -rf logdir/procgen/ && python -m dreamer.scripts.train --logdir ./logdir/procgen --params '{defaults: [dreamer, pcont, discrete, atari], tasks: [atari_boxing]}'
-rm -rf logdir/procgen/ && python -m dreamer.scripts.train --logdir ./logdir/procgen --params '{defaults: [dreamer, pcont, discrete], tasks: [procgen-jumper]}'
+python -m dreamer.scripts.train --logdir ./logdir/procgen --params '{defaults: [dreamer, pcont, discrete], tasks: [procgen-jumper]}' --resume_runs False
